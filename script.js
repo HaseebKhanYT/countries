1bd2e01a0f97f7a95d11cc24e39633960ddf85bc
Haseeb Khan <haseebkhanyt@hotmail.com>
2024-03-24 21:53:47 -0700

    feat: adding 4 countries on the page

---
+++ script.js
@@ -0,0 +1,215 @@
+// Add more elements to these lists. The contents do not have to be real.
+const GEOGRAPHY_LIST = [
+  {
+    country: "Saudi Arabia",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Flag_of_Saudi_Arabia.svg",
+    about:
+      "Saudi Arabia, officially the Kingdom of Saudi Arabia (KSA), is a country in West Asia and the Middle East. It covers the bulk of the Arabian Peninsula and has a land area of about 2150000 (sq km) (830000 sq mi), making it the fifth-largest country in Asia and the largest in the Middle East. Living in Saudi Arabia offers several positive aspects. The country boasts a rich cultural heritage deeply rooted in Islamic traditions, providing residents with opportunities to engage in diverse cultural experiences and celebrations. Saudi Arabia's robust economy, largely fueled by its vast oil reserves, ensures economic stability and offers a high standard of living for many residents. The modern infrastructure, including well-developed transportation systems and state-of-the-art healthcare facilities, enhances convenience and quality of life. Moreover, the government's focus on education has led to the establishment of numerous universities and colleges, providing access to quality education for both citizens and expatriates. Additionally, Saudi Arabia's strategic location in the Middle East offers residents the chance to explore neighboring countries and experience a variety of landscapes, from deserts to coastlines, contributing to a rich and fulfilling lifestyle for those who call the Kingdom home.",
+    officialName: "Kingdom of Saudi Arabia",
+    capital: "Riyadh",
+    largestCity: "Riyadh",
+    subdivisionName: "Provinces",
+    subdivisions: [
+      {
+        name: "Eastern Province",
+        capital: "Dammam",
+        largestCity: "Dammam",
+        area: 672522,
+      },
+      {
+        name: "Riyadh Province",
+        capital: "Riyadh",
+        largestCity: "Riyadh",
+        area: 404240,
+      },
+      {
+        name: "Makkah Province",
+        capital: "Makkah",
+        largestCity: "Jeddah",
+        area: 153148,
+      },
+    ],
+  },
+  {
+    country: "United States",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg",
+    about:
+      "The United States of America (USA or U.S.A.), commonly known as the United States (US or U.S.) or America, is a country primarily located in North America, between Canada and Mexico. It is a federation of 50 states, a federal capital district (Washington, D.C.), and 326 Indian reservations. Outside the union of states, it asserts sovereignty over five major unincorporated island territories and various uninhabited islands. The country has the world's third-largest land area, largest maritime exclusive economic zone, and the third-largest population, exceeding 334 million. The country's diverse cultural landscape provides residents with exposure to a wide array of traditions, cuisines, and perspectives, fostering a vibrant and inclusive society. The U.S. economy is one of the largest and most dynamic in the world, offering abundant opportunities for career advancement, entrepreneurship, and innovation. Its world-class healthcare system ensures access to quality medical care, supported by cutting-edge research and technology. The United States is renowned for its top-tier educational institutions, from prestigious universities to specialized vocational training programs, enabling individuals to pursue their academic and professional aspirations. Furthermore, the country's breathtaking natural beauty, diverse climates, and varied landscapes—from majestic national parks to bustling urban centers—offer residents unparalleled recreational and lifestyle options, making it an attractive destination for people from all walks of life.",
+    officialName: "United States of America",
+    capital: "Washington",
+    largestCity: "New York",
+    subdivisionName: "States",
+    subdivisions: [
+      {
+        name: "California",
+        capital: "Sacramento",
+        largestCity: "Los Angeles",
+        area: 423970,
+      },
+      {
+        name: "Nevada",
+        capital: "Carson City",
+        largestCity: "Las Vegas",
+        area: 286382,
+      },
+      {
+        name: "Louisiana",
+        capital: "Baton Rouge",
+        largestCity: "New Orleans",
+        area: 134264,
+      },
+    ],
+  },
+  {
+    country: "India",
+    flag: "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+    officialName: "Republic of India",
+    about:
+      "India, officially the Republic of India, is a country in South Asia. It is the seventh-largest country by area; the most populous country as of June 2023; and from the time of its independence in 1947, the world's most populous democracy. India is renowned for its vibrant cultural tapestry, where residents can immerse themselves in a diverse array of traditions, languages, and cuisines. The country's democratic ethos fosters a sense of inclusivity and freedom of expression, allowing individuals to engage actively in civic life and contribute to the nation's dynamic social fabric. India's rapidly growing economy presents ample opportunities for professional and entrepreneurial growth, particularly in sectors such as technology, healthcare, and education. The country's rich history and architectural marvels, from ancient monuments to bustling modern cities, provide a unique backdrop for exploration and cultural enrichment. Additionally, India's natural beauty, spanning from the majestic Himalayas to the serene backwaters of Kerala, offers residents opportunities for adventure and relaxation amidst breathtaking landscapes. Overall, living in India offers a vibrant and enriching experience, characterized by its cultural diversity, economic dynamism, and natural splendor.",
+    capital: "New Delhi",
+    largestCity: "Mumbai",
+    subdivisionName: "States",
+    subdivisions: [
+      {
+        name: "Telangana",
+        capital: "Hyderabad",
+        largestCity: "Hyderabad",
+        area: 112077,
+      },
+      {
+        name: "Maharashtra",
+        capital: "Mumbai",
+        largestCity: "Mumbai",
+        area: 307713,
+      },
+      {
+        name: "Kerala",
+        capital: "Thiruvananthapuram",
+        largestCity: "Thiruvananthapuram",
+        area: 38863,
+      },
+    ],
+  },
+  {
+    country: "United Arab Emirates",
+    flag: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_United_Arab_Emirates.svg",
+    about:
+      "The United Arab Emirates (UAE), or simply the Emirates, is a country in West Asia, in the Middle East. It is located at the eastern end of the Arabian Peninsula and shares borders with Oman and Saudi Arabia, while also having maritime borders in the Persian Gulf with Qatar and Iran. Abu Dhabi is the country's capital, while Dubai, the most populous city, is an international hub. The UAE is renowned for its modern and cosmopolitan lifestyle, with vibrant cities like Dubai and Abu Dhabi boasting world-class amenities, luxurious accommodations, and diverse cultural experiences. The country's booming economy, driven by sectors such as finance, tourism, and trade, provides residents with ample career opportunities and a high standard of living. The UAE's commitment to innovation and infrastructure development is evident in iconic landmarks like the Burj Khalifa and ambitious projects such as the Expo 2020 site, fostering a dynamic and progressive environment. Moreover, the UAE's tax-free income and business-friendly policies attract expatriates from around the globe, creating a multicultural society that celebrates diversity. With its stunning beaches, desert landscapes, and modern leisure attractions, the UAE offers residents a balanced lifestyle, blending work and play seamlessly.",
+    officialName: "United Arab Emirates",
+    capital: "Abu Dhabi",
+    largestCity: "Dubai",
+    subdivisionName: "Emirates",
+    subdivisions: [
+      {
+        name: "Abu Dhabi",
+        capital: "Abu Dhabi",
+        largestCity: "Abu Dhabi",
+        area: 972,
+      },
+      {
+        name: "Dubai",
+        capital: "Dubai",
+        largestCity: "Dubai",
+        area: 1610,
+      },
+      {
+        name: "Sharjah",
+        capital: "Sharjah",
+        largestCity: "Sharjah",
+        area: 235,
+      },
+    ],
+  },
+];
+
+// Your code goes below
+
+const root = document.querySelector("#root");
+
+root.insertAdjacentHTML("afterbegin", "<ol>");
+
+const rootOl = document.querySelector("#root > ol");
+
+for (let countries of GEOGRAPHY_LIST) {
+  const divisions = countries.subdivisions;
+  let subdivisionTemplate = ``;
+  divisions.forEach((element) => {
+    subdivisionTemplate += `<th scope="row">${element.name}:</th>
+    <td>
+      <tr>
+        <td>Capital City:</td>
+        <td>${element.capital}</td>
+      </tr>
+      <tr>
+        <td>Largest City:</td>
+        <td>${element.largestCity}</td>
+      </tr>
+      <tr>
+        <td>Area (sq km):</td>
+        <td>${element.area}</td>
+      </tr>
+    </td>`;
+  });
+
+  rootOl.insertAdjacentHTML(
+    "beforeend",
+    `<li><header><h2>${countries.country}</h2></header>
+    <main>
+    <p>
+        <img
+            src="${countries.flag}"
+            alt="Flag of ${countries.country}"
+            height="150"
+            width="225"
+            style="float: left; margin: 0px 10px 10px 0px"
+        />
+        ${countries.about}
+    </p>
+    <table
+  >
+    <tr>
+      <th style="text-align: center" colspan="2">
+        Some key information about ${countries.country}
+      </th>
+    </tr>
+    <tbody>
+      <tr>
+        <td style="flex: 1 0">Official Name:</td>
+        <td>${countries.officialName}</td>
+      </tr>
+      <tr>
+        <td>Capital City:</td>
+        <td>${countries.capital}</td>
+      </tr>
+      <tr>
+        <td>Largest City:</td>
+        <td>${countries.largestCity}</td>
+      </tr>
+      <tr>
+        <th>${countries.subdivisionName}</th>
+      </tr>
+      <tr >` +
+      subdivisionTemplate +
+      `
+
+      </tr>
+    </tbody>
+    <caption>
+      Source:
+      <cite
+        ><a
+          href="https://en.wikipedia.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Wikipedia</a
+        ></cite
+      >
+    </caption>
+  </table>
+
+    </main>
+    </li>`
+  );
+}
+
+root.insertAdjacentHTML("beforeend", "</ol>");
